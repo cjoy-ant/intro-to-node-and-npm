0629e4c4a7c0783ebd8f08a778a463db6960ed0d
Christine Antonio <cjoy.antonio@gmail.com>
2021-02-17 17:53:17 -0600

    starter code

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(process.argv)
+let actualHours = 45;
+let hourlyRate = 50;
+let overtime = 1.5;
+let weeklyHours = 40;
+
+let overtimeHours = actualHours > weeklyHours ? actualHours - weeklyHours : 0;
+let normalHours = actualHours > weeklyHours ? weeklyHours : actualHours;
+let normalPay = normalHours * hourlyRate;
+let overtimePay = overtimeHours * hourlyRate * overtime;
+
+let totalPay = normalPay + overtimePay;
+
+console.log(`The total pay is $${totalPay}`);
